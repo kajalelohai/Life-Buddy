91ddc6983c2c58864bb82cf47db5e3561591baf1
Kajal Negi <27279935+kajalelohai@users.noreply.github.com>
2018-04-01 21:25:57 +0530

    feat: add faded class

--- src/views/App/index.js
+++ src/views/App/index.js
@@ -1,6 +1,37 @@
 import React from 'react';
 import './style.css';
 
-const App = () => {return <div> Hello World </div>}
+class App extends React.Component {
+  state = { answer: '' };
+
+  handleChangeAnswer = (event) => {
+    const answer = event.target.value.substr(0, 20);
+
+    this.setState({ answer });
+  };
+
+  render = () => {
+    let questionClass = 'question';
+
+    if (this.state.answer !== '') {
+      questionClass += ' faded';
+    }
+
+    return (
+      <div className="container">
+        <div className={questionClass}>How would you like to feel at the end of the day?</div>
+        <div className="answer">
+          <input
+            id="answerId"
+            className="input"
+            type="text"
+            value={this.state.answer}
+            onChange={this.handleChangeAnswer}
+          />
+        </div>
+      </div>
+    );
+  };
+}
 
 export default App;
